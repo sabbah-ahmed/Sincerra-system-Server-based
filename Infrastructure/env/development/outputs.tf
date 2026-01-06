8eb01883d7ec8e4cd2f87c78b7f8ec3e7924271b
Ahmed abdullah <aa1392002@gmail.com>
2026-01-06 22:44:03 +0200

    before changing scripts

--- Infrastructure/env/development/outputs.tf
+++ Infrastructure/env/development/outputs.tf
@@ -1,4 +1,11 @@
-output "alb_dns_name" {
+output "presentation_alb_dns_name" {
   description = "DNS name of the Application Load Balancer - Use this URL to access your frontend"
   value       = module.alb.presentation_alb_dns
 }
+
+
+output "backend_alb_dns_name" {
+  description = "DNS name of the Application Load Balancer - Use this URL to access your frontend"
+  value       = module.backend_alb.backend_alb_dns
+}
+
